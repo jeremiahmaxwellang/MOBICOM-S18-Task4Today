4e58512f7cf4e06e4ee8a559e754a7da371bbbdf
juiceticedlsu <136901079+juiceticedlsu@users.noreply.github.com>
2025-07-08 17:31:07 +0800

    front end updated - can now navigate between pages
    future update
    - will try to make it prettier next

--- app/src/main/java/com/mobdeve/s18/task4today/CalendarFragment.kt
+++ app/src/main/java/com/mobdeve/s18/task4today/CalendarFragment.kt
@@ -6,34 +6,27 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.mobdeve.s18.task4today.databinding.FragmentTaskListBinding
+import com.mobdeve.s18.task4today.databinding.FragmentCalendarBinding
 
-class TaskListFragment : Fragment() {
+class CalendarFragment : Fragment() {
 
-    private var _binding: FragmentTaskListBinding? = null
+    private var _binding: FragmentCalendarBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentTaskListBinding.inflate(inflater, container, false)
+        _binding = FragmentCalendarBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.addTaskButton.setOnClickListener {
-            Toast.makeText(requireContext(), "Add Task clicked", Toast.LENGTH_SHORT).show()
-        }
-
-        binding.prevDayBtn.setOnClickListener {
-            Toast.makeText(requireContext(), "Previous day", Toast.LENGTH_SHORT).show()
-        }
-
-        binding.nextDayBtn.setOnClickListener {
-            Toast.makeText(requireContext(), "Next day", Toast.LENGTH_SHORT).show()
+        binding.calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            val date = "$dayOfMonth/${month + 1}/$year"
+            Toast.makeText(requireContext(), "Selected: $date", Toast.LENGTH_SHORT).show()
         }
     }
 
