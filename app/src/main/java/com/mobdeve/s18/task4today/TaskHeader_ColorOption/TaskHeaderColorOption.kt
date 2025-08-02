84f729d25ed30592bb290048122a9bd2122206a5
Charles <charles_duelas@dlsu.edu.ph>
2025-08-02 11:51:27 +0800

    Delete app directory

--- app/src/main/java/com/mobdeve/s18/task4today/TaskHeader_ColorOption/TaskHeaderColorOption.kt
+++
@@ -1,8 +0,0 @@
-package com.mobdeve.s18.task4today.TaskHeader_ColorOption
-
-import com.mobdeve.s18.task4today.TaskHeader_ColorOption.TaskHeaderColorOption
-
-data class TaskHeaderColorOption(
-    val name: String,
-    val hex: String
-)
